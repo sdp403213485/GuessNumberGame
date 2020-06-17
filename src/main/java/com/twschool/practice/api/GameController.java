d5120d028c636972b9bc23ff18a2f4f0e915b9bd
sdp <403213485@qq.com>
2020-06-17 21:56:20 +0800

    用户赢一次加三分输一次减三分

--- src/main/java/com/twschool/practice/api/GameController.java
+++ src/main/java/com/twschool/practice/api/GameController.java
@@ -3,6 +3,7 @@ package com.twschool.practice.api;
 import com.twschool.practice.domain.GameStatus;
 import com.twschool.practice.domain.GuessNumberGame;
 import com.twschool.practice.domain.RandomAnswerGenerator;
+import com.twschool.practice.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,6 @@ import java.util.Map;
 
 @RestController
 public class GameController {
-    RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
-    GuessNumberGame guessNumberGame = new GuessNumberGame(randomAnswerGenerator);
 
     @GetMapping("/game")
     public Map<String, String> guess(@RequestParam String guess) {
@@ -33,23 +32,36 @@ public class GameController {
 //        return 1;
 //    }
 
-        @GetMapping("/OneWhetherWon")
-    public int OneWhetherWon(@RequestParam String userAnswerString){
-        int  score = 0;
+        @GetMapping("/OneWhetherWn")
+        public int OneWhetherWon(@RequestParam String userAnswerString,@RequestParam User user){
+        user.setTotalScore(0);
+        RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
+        GuessNumberGame guessNumberGame = new GuessNumberGame(randomAnswerGenerator);
         List<String> userAnswer = Arrays.asList(userAnswerString.split(" "));
             guessNumberGame.guess(userAnswer);
             if (guessNumberGame.getStatus().equals(GameStatus.SUCCEED)){
-                score = score +3;
-            }else if(guessNumberGame.getStatus().equals(GameStatus.FAILED) && guessNumberGame.getLeftTryTimes() == 0) {
-                score = score - 3;
-            }else{
-                score = score;
+                user.setTotalScore(user.getTotalScore()+3);
+            }else if(guessNumberGame.getStatus().equals(GameStatus.FAILED) ) {
+                user.setTotalScore(user.getTotalScore()-3);;
             }
-        return score;
+        return user.getTotalScore();
     }
+//    public int OneWhetherWon(@RequestParam String userAnswerString){
+////        int  score = 0;
+////        List<String> userAnswer = Arrays.asList(userAnswerString.split(" "));
+////            guessNumberGame.guess(userAnswer);
+////            if (guessNumberGame.getStatus().equals(GameStatus.SUCCEED)){
+////                score = score +3;
+////            }else if(guessNumberGame.getStatus().equals(GameStatus.FAILED) && guessNumberGame.getLeftTryTimes() == 0) {
+////                score = score - 3;
+////            }else{
+////                score = score;
+////            }
+////        return score;
+////    }
 
 //    @GetMapping("/OneGameWhetherWon")
-//    public int OneGameWhetherWon(){
+//    public int OneGameWhetherWon(@RequestParam User user,@RequestParam String guess){
 //        RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
 //        GuessNumberGame guessNumberGame = new GuessNumberGame(randomAnswerGenerator);
 //        for(int i = 0; i <= guessNumberGame.getMAX_TRY_TIMES(); i++){
